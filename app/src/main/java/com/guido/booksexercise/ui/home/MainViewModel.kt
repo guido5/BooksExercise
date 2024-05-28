21310d80277cb538c9f13a284e9713857285cc51
Diego Guido <44814194+guido5@users.noreply.github.com>
2024-05-28 13:07:26 -0600

    Update MainViewModel.kt

--- app/src/main/java/com/guido/booksexercise/ui/home/MainViewModel.kt
+++ app/src/main/java/com/guido/booksexercise/ui/home/MainViewModel.kt
@@ -3,8 +3,12 @@ package com.guido.booksexercise.ui.home
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.guido.booksexercise.data.Books
 import com.guido.booksexercise.data.retrofitRepository.VolumesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -14,10 +18,13 @@ class MainViewModel
 @Inject constructor(app: Application, val volumesRepository: VolumesRepository) : AndroidViewModel(app) {
 
 
-    fun getVolumes() {
+    val _searchedItems =  MutableStateFlow<List<Books>>(listOf())
+    val searchedItems = _searchedItems.asStateFlow()
+
+    fun getVolumes(title : String) {
         viewModelScope.launch {
-            volumesRepository.getVolumes("harry potter").collect {
-                println(it.size)
+            volumesRepository.getVolumes(title).collect {
+                _searchedItems.value = it
             }
         }
     }
